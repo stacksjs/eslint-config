3f0eebe672225f9a8be2f238b7758e00ff289f85
Chris <chrisbreuer93@gmail.com>
2024-10-11 20:47:46 +0200

    chore: wip

--- src/configs/vue.ts
+++ src/configs/vue.ts
@@ -34,6 +34,7 @@ export async function vue(
     parserVue,
     processorVueBlocks,
   ] = await Promise.all([
+    // @ts-expect-error missing types
     interopDefault(import('eslint-plugin-vue')),
     interopDefault(import('vue-eslint-parser')),
     interopDefault(import('eslint-processor-vue-blocks')),
