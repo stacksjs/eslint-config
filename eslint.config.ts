cc8d92ffe1c9e9c647d2bcb0aadc19bfcde1f040
Chris <chrisbreuer93@gmail.com>
2025-05-17 15:48:15 -0700

    chore: initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,41 @@
+import styleMigrate from '@stylistic/eslint-plugin-migrate'
+
+import { antfu } from './src'
+
+export default antfu(
+  {
+    vue: {
+      a11y: true,
+    },
+    react: true,
+    solid: true,
+    svelte: true,
+    astro: true,
+    typescript: true,
+    formatters: true,
+    pnpm: true,
+    type: 'lib',
+  },
+  {
+    ignores: [
+      'fixtures',
+      '_fixtures',
+      '**/constants-generated.ts',
+    ],
+  },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'perfectionist/sort-objects': 'error',
+    },
+  },
+  {
+    files: ['src/configs/*.ts'],
+    plugins: {
+      'style-migrate': styleMigrate,
+    },
+    rules: {
+      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+    },
+  },
+)
---
+++ src/cli/constants-generated.ts
@@ -0,0 +1,15 @@
+export const versionsMap = {
+  "@eslint-react/eslint-plugin": "^1.49.0",
+  "@unocss/eslint-plugin": "^66.1.2",
+  "astro-eslint-parser": "^1.2.2",
+  "eslint": "^9.26.0",
+  "eslint-plugin-astro": "^1.3.1",
+  "eslint-plugin-format": "^1.0.1",
+  "eslint-plugin-react-hooks": "^5.2.0",
+  "eslint-plugin-react-refresh": "^0.4.20",
+  "eslint-plugin-solid": "^0.14.5",
+  "eslint-plugin-svelte": "^3.7.0",
+  "prettier-plugin-astro": "^0.14.1",
+  "prettier-plugin-slidev": "^1.0.5",
+  "svelte-eslint-parser": "^1.2.0"
+}
